74dc487fa970953e6d3e4636e7a475f5ad9b42aa
Ofer Tauber <ofertauber@gmail.com>
2022-10-25 22:14:33 +0300

    Partly functioning version

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,62 +1,86 @@
-import { useEffect, useState } from 'react';
 import CharacterCard from './Card';
 import Character from '../utiles/CarecterDTO';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
+import { useInfiniteQuery } from 'react-query';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import LinearProgress from '@mui/material/LinearProgress';
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
+
+const Loader = () => {
+  return (
+    <Box sx={{ width: '100%' }}>
+      <br />
+      <LinearProgress />
+      <br />
+    </Box>
+  );
+};
 
 const List = () => {
-  const [a, setA] = useState<false | Character>(false);
+  const { ref, inView } = useInView();
 
-  const fetchCahr = async (): Promise<void> => {
-    try {
-      const res = await fetch('https://rickandmortyapi.com/api/character/52');
-      const character: Character = await res.json();
+  const fetchCharacters = async ({
+    pageParam = 'https://rickandmortyapi.com/api/character?page=1',
+  }) => {
+    const res = await fetch(pageParam);
+    return await res.json();
+  };
 
-      setA(character);
-    } catch (e) {
-      console.error(e);
+  const { data, error, fetchNextPage, isFetching, status } = useInfiniteQuery(
+    ['RickAndMorty'],
+    fetchCharacters,
+    {
+      getNextPageParam: (lastPage, pages) => {
+        void pages;
+        return lastPage.info.next;
+      },
     }
+  );
+
+  const mapCards = () => {
+    if (!data) return [];
+
+    const pages: Array<any> = data.pages;
+    return pages.reduce((acc, cur) => {
+      const cards = acc.concat(
+        cur.results.map((char: Character) => (
+          <CharacterCard key={char.id} character={char} />
+        ))
+      );
+      const refDiv = <div ref={ref} key={'dummy-ref' + cards.length}></div>;
+      cards.splice(cards.length - 3, 0, refDiv);
+      return cards;
+    }, []);
   };
 
   useEffect(() => {
-    fetchCahr();
-  }, []);
+    if (inView) fetchNextPage();
+  }, [inView, fetchNextPage]);
 
-  if (!a) return <div>List</div>;
+  if (status === 'loading') return <div>Loading...</div>;
+
+  if (status === 'error') {
+    console.log(error);
+    return <div>Error!</div>;
+  }
 
   return (
     <>
+      <br />
       <CssBaseline />
       <Container maxWidth="md">
         <Grid container spacing={4}>
-          <CharacterCard character={a} />
-          <CharacterCard character={a} />
-          <CharacterCard character={a} />
-          <CharacterCard character={a} />
-          <CharacterCard character={a} />
-          <CharacterCard character={a} />
-          <CharacterCard character={a} />
-          <CharacterCard character={a} />
-          <CharacterCard character={a} />
+          {mapCards()}
         </Grid>
+        {isFetching && <Loader />}
       </Container>
     </>
   );
 };
 
 export default List;
-
-// import * as React from 'react';
-
-// import Box from '@mui/material/Box';
-
-// export default function SimpleContainer() {
-//   return (
-//     <React.Fragment>
-//       <CssBaseline />
-//       <Container maxWidth="sm">
-//         <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
-//       </Container>
-//     </React.Fragment>
-//   );
